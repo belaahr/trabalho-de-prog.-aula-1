e7421a2bb1e880f2623405f58f760e51b3139bd7
belaahr <111088055+belaahr@users.noreply.github.com>
2024-03-27 07:47:12 -0300

    fgdfhdfh

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 alert('Olá, seja muito bem vindo');
 
 let nomeUsuario = prompt('cuidado com o 🐋 qual seu nome?');
+
+let texto = document.querySelector('span');
+
+texto.textContent = nomeUsuario; 
