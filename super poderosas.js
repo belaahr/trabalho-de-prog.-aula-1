f9209b09d9a463e3c7c2c3da25c3669a195aad65
belaahr <111088055+belaahr@users.noreply.github.com>
2024-04-22 11:35:19 -0300

    yeryet

--- super poderosas.js
+++ super poderosas.js
@@ -22,7 +22,7 @@ const tempoObjetivo1 = new Date('2025-03-16T00:00:00');
 let TempoAtual = new Date ();
 let tempoFinal = tempoObjetivo1 - TempoAtual;
 let segundos = Math.floor (tempoFinal/1000);
-let minutos = Math.floor (tempoFinal/60);
+let minutos = Math.floor (segundos/60);
 let horas = Math.floor (minutos/60);
 let dias = Math.floor (horas/24);
 
